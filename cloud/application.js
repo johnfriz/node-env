d7673f70296a9ff208543fe8e8139369a1f7a814
John Frizelle <john.frizelle@feedhenry.com>
2013-10-09 21:58:31 +0100

    Adding default FH Files

--- cloud/application.js
+++ cloud/application.js
@@ -1,66 +1,16 @@
-var http = require("http"),
-    url = require("url"),
-    path = require("path"),
-    fs = require("fs"),
-    mustache = require("./lib/mustache"),
-    keyprocessor = require("./lib/keyprocessor"),
-    sysinfo = require('./lib/sysinfo'),
-    port = process.env.PORT || 8888;
+var webapp = require('fh-webapp');
+var express = require('express');
+$fh = require('fh-api');
+var mainjs = require('main.js');
 
-http.createServer(function(request, response) {
+var app = express();
+app.use('/sys', webapp.sys(mainjs));
+app.use('/mbaas', webapp.mbaas);
+app.use('/cloud', webapp.cloud(mainjs));
 
-    var uri = url.parse(request.url).pathname,
-        filename = path.join(process.cwd(), uri);
+// You can define custom URL handlers here, like this one:
+app.use('/', function(req, res){
+  res.end('Your Cloud App is Running');
+});
 
-    path.exists(filename, function(exists) {
-        if (!exists) {
-            response.writeHead(404, {
-                "Content-Type": "text/plain"
-            });
-            response.write("404 Not Found\n");
-            response.end();
-            return;
-        }
-
-        if (uri == "/") {
-            var t = path.join(filename, '/index.html');
-            var v = {
-                env: [],
-                sys: sysinfo.sysInfo()
-            };
-            for (var item in process.env) {
-                v['env'].push({
-                    'key': item,
-                    'value': keyprocessor.procKey(item, process.env[item])
-                });
-            }
-
-            fs.readFile(t, 'utf8', function(err, data) {
-                var html = mustache.to_html(data, v);
-                response.writeHead(200);
-                response.write(html, "binary");
-                response.end()
-                return;
-            });
-        }
-
-        if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-
-        fs.readFile(filename, "binary", function(err, file) {
-            if (err) {
-                response.writeHead(500, {
-                    "Content-Type": "text/plain"
-                });
-                response.write(err + "\n");
-                response.end();
-                return;
-            }
-
-            response.writeHead(200);
-            response.write(file, "binary");
-            response.end();
-        });
-    });
-}).listen(parseInt(port, 10));
-
-console.log("Server running at\n  => http://0.0.0.0:" + port + "/\nCTRL + C to shutdown");
+module.exports = app.listen(process.env.FH_PORT || process.env.VCAP_APP_PORT || 8001);
